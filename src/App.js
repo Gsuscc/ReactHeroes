0ed809ebe349e446f089db52638002b3e2208f2e
ZsoltTirkala <tirkala.zsolt@gmail.com>
2020-09-21 20:26:40 +0200

    added heading with style component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { useContext } from "react";
 import { GlobalContext } from "./GlobalState";
 import { HeroesList } from "./components/HeroesList";
+import styled from "styled-components";
 
 import "./App.css";
+
+const H1 = styled.h1`
+  font-family: superHeroFont;
+  font-size: 3.5rem;
+  text-align: center;
+  color: brown;
+`;
 
 function App() {
   return (
     <div className="App">
+      <H1>Heroes of React</H1>
       <HeroesList />
     </div>
   );
