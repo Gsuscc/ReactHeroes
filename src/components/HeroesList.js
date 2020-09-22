9d4d1a2be097e4bc3417562d707b382519fab901
Gergely Molnar <molnar.gergo18@gmail.com>
2020-09-22 17:01:20 +0200

    add infinte scroll feature

--- src/components/HeroesList.js
+++ src/components/HeroesList.js
@@ -1,20 +1,51 @@
-import React, { useContext } from "react";
+import React, { useCallback, useState, useRef, useEffect } from "react";
 import { GlobalContext } from "../GlobalState";
 import { HeroCard } from "./HeroCard";
 
 export const HeroesList = (props) => {
-  const { heroId } = useContext(GlobalContext);
-  const [id, setId] = heroId;
+  // const { heroId } = useContext(GlobalContext);
+  const [lastId, setLastId] = useState(18);
+  const [heroesList, setHeroesList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [id, setId] = useState(1);
 
-  let page = 0;
+  const observer = useRef();
+  const lastCard = useCallback(
+    (card) => {
+      console.log(card);
+      if (isLoading) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && id < 731) {
+          console.log("visivle");
+          setLastId((lastId) => lastId + 18);
+        }
+      });
+      if (card) observer.current.observe(card);
+      console.log("last");
+    },
+    [id, isLoading]
+  );
 
-  const getHeroes = () => {
-    let heroesList = [];
-    for (let i = page * 10 + 1; i < page * 10 + 19; i++) {
-      heroesList.push(<HeroCard id={i} key={i} />);
+  useEffect(() => {
+    setIsLoading(true);
+    let newList = [];
+    let currentId = id;
+    while (currentId <= lastId) {
+      if (currentId === lastId) {
+        newList.push(
+          <div ref={lastCard}>
+            <HeroCard id={currentId} key={currentId} />
+          </div>
+        );
+      } else {
+        newList.push(<HeroCard id={currentId} key={currentId} />);
+      }
+      currentId++;
     }
-    return heroesList;
-  };
+    setHeroesList((heroesList) => [...heroesList, newList]);
+    setId((id) => (id = currentId));
+  }, [id, lastCard, lastId]);
 
-  return <div className="heroContainer">{getHeroes().map((hero) => hero)}</div>;
+  return <div className="heroContainer">{heroesList.map((hero) => hero)}</div>;
 };
