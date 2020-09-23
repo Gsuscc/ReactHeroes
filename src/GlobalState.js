7d71c4e831a032713f4afb033cd82c95aa14166f
Gergely Molnar <molnar.gergo18@gmail.com>
2020-09-23 06:36:49 +0200

    optimize imports

--- src/GlobalState.js
+++ src/GlobalState.js
@@ -1,9 +1,7 @@
-import React, { createContext, useState, useEffect } from "react";
-import axios from "axios";
+import React, { createContext, useState } from "react";
 export const GlobalContext = createContext();
 
 export const GlobalState = (props) => {
-  const [heroes, setHeroes] = useState([]);
   const [id, setId] = useState(1);
   return (
     <GlobalContext.Provider value={{ heroId: [id, setId] }}>
