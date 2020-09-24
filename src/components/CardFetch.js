f0b90aeb19ddb5394b1ab44de8eaf6228ef3487e
ZsoltTirkala <tirkala.zsolt@gmail.com>
2020-09-25 00:10:31 +0200

    fixing delete proxy

--- src/components/CardFetch.js
+++ src/components/CardFetch.js
@@ -2,9 +2,7 @@ import axios from "axios";
 
 export default async function cardFetch(id, callback) {
   axios
-    .get(
-      `http://atib.servebeer.com:55555/https://superheroapi.com/api/3597117540350761/${id}`
-    )
+    .get(`https://superheroapi.com/api/3597117540350761/${id}`)
     .then((response) => {
       let hero = response.data;
       if (callback) callback(hero);
