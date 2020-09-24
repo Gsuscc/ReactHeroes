c80c38419cf279b8480d9887681bb247e192cd78
attila-h <hauth.attila@gmail.com>
2020-09-25 00:16:45 +0200

    remove css card fade at arena

--- src/components/CardFetch.js
+++ src/components/CardFetch.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 export default async function cardFetch(id, callback) {
   axios
-    .get(`https://superheroapi.com/api/3597117540350761/${id}`)
+    .get(
+      `http://atib.servebeer.com:55555/https://superheroapi.com/api/3597117540350761/${id}`
+    )
     .then((response) => {
       let hero = response.data;
       if (callback) callback(hero);
