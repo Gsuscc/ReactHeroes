37836465e325e8af8aaf0203cf2360f46ccf526a
Gergely Molnar <molnar.gergo18@gmail.com>
2020-12-02 16:13:37 +0100

    change fetch url

--- src/components/CardFetch.js
+++ src/components/CardFetch.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default async function cardFetch(id, callback) {
   axios
     .get(
-      `http://atib.servebeer.com:55555/https://superheroapi.com/api/3597117540350761/${id}`
+      `http://localhost:8762/api/hero/get/${id}`
     )
     .then((response) => {
       let hero = response.data;
